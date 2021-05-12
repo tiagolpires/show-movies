705bab7fe8d7b2fd44889dcba38de3ceca4d5d4a
Tiago Pires <tiagolpires@hotmail.com>
2021-05-12 08:46:36 -0300

    Dropdown Menu on Filters

--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -9,17 +9,32 @@ const index = () => {
                 <div className="catalog" id="catalog">
                     <div className="catalog-filters">
                         <div className="catalog-left-filters">
-                            <div className="catalog-filter">
-                                <img src={downArrow} alt="down" />
-                                <span>por gênero</span>
+                            <div className="catalog-filter-container">
+                                <div className="catalog-filter-btn">
+                                    <img src={downArrow} alt="down" />
+                                    <span>por gênero</span>
+                                </div>
+                                <ul className="catalog-filter-dropdown-content">
+                                    <li>Ação</li>
+                                    <li>Drama</li>
+                                    <li>Comédia</li>
+                                    <li>Romance</li>
+                                    <li>Aventura</li>
+                                </ul>
                             </div>
                             <div className="catalog-filter-trending selected">
                                 <span>mais populares</span>
                             </div>
                         </div>
-                        <div className="catalog-filter">
-                            <img src={downArrow} alt="down" />
-                            <span>em lista</span>
+                        <div className="catalog-filter-container">
+                            <div className="catalog-filter-btn">
+                                <img src={downArrow} alt="down" />
+                                <span>em lista</span>
+                            </div>
+                            <ul className="catalog-filter-dropdown-content">
+                                <li>Grid</li>
+                                <li>Lista</li>
+                            </ul>
                         </div>
                     </div>
                     <div className="catalog-movies-container wrap">
