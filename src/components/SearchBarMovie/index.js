7e2b74449d65ceb28c8111401616bb5a551a9a4c
Tiago Pires <tiagolpires@hotmail.com>
2021-05-13 22:45:38 -0300

    Header Responsiviness

--- src/components/SearchBarMovie/index.js
+++ src/components/SearchBarMovie/index.js
@@ -8,7 +8,7 @@ const index = ({ movie: {title, vote_average: rate, id, poster_path: posterPath,
             <div className="search-bar-movie" onClick={() => setIsSearchBarActive(false)}>
                 <div className="search-bar-movie-image" style={{backgroundImage: `url(https://image.tmdb.org/t/p/w200${posterPath})`}}></div>
                 <div className="search-bar-movie-info">
-                    <h4>{title.length <= 13 ? title : title.substr(0, 13) + '...'}</h4>
+                    <h4>{title.length <= 18 ? title : title.substr(0, 18) + '...'}</h4>
                     <span className="search-bar-movie-info-gender">{genres.length > 0 && genres[0]}</span>
                     <div className="search-bar-movie-rate-container">
                         <img src={rateStar} alt="rate-star"/>
